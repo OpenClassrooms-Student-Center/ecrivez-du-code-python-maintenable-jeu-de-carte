b454ebeac60e0b1011cca684f115058d8ed47c39
mikael <mbriolet.ma@gmail.com>
2021-04-10 09:49:28 +0200

    p3c6s3
    + add the client code

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 """Entry point."""
 
-from models.card import Card
 from models.deck import Deck
-from models.player import Player
+from controllers.base import Controller
+from views.base import View
+
+
+def main():
+    deck = Deck()
+    view = View()
+    game = Controller(deck, view)
+    game.run()
 
 
 if __name__ == "__main__":
-    card1 = Card("diamonds", "cinq")
-    card2 = Card("coeurs", "cinq")
-    card3 = Card("coeurs", "valet")
-    print(card1 < card2)
-    print(card3 > card2)
-
-    deck = Deck()
-    print(deck)
-
-    player = Player("John")
-    player.hand.append(deck.draw_card())
-    print(player.hand)
-
-    # On ne peut pas encore tester notre controller. ;)
+    main()
