45490756d549f0a5c9997a5fce5d60dc9e16e206
daniel-ob <danobrey@gmail.com>
2021-07-27 13:28:14 +0200

    Fix issue in check() cards method

--- controllers/evaluate.py
+++ controllers/evaluate.py
@@ -11,25 +11,22 @@ class CheckerRankAndSuitIndex:
 
     def check(self, players: List[Player]):
         """Start the evaluation."""
-        last_player = players[0]
         best_candidate = players[0]
 
         for player in players[1:]:
             player_card = player.hand[0]
-            last_player_card = last_player.hand[0]
+            best_candidate_card = best_candidate.hand[0]
 
             score = (RANKS.index(player_card.rank), SUITS.index(player_card.suit))
-            last_score = (
-                RANKS.index(last_player_card.rank),
-                SUITS.index(last_player_card.suit),
+            best_score = (
+                RANKS.index(best_candidate_card.rank),
+                SUITS.index(best_candidate_card.suit),
             )
 
-            if score[0] == last_score[0]:
-                if score[1] > last_score[1]:
+            if score[0] == best_score[0]:
+                if score[1] > best_score[1]:
                     best_candidate = player
-            elif score[0] > last_score[0]:
+            elif score[0] > best_score[0]:
                 best_candidate = player
-
-            last_player = player
 
         return best_candidate.name
